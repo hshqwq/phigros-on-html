c1044563259d87302b5e5e2b93d5ffc99b085f53
hshqwq <yxr080215@163.com>
2022-03-10 23:32:00 +0800

    main files

--- asset/js/game.js
+++ asset/js/game.js
@@ -7,30 +7,32 @@ function gaming() {
     var audio;
     function loadGame({music, bg = 'none', songName='?', author='unknown', tDfcy='EZ', dfcy='?'}) {
         audio = new Audio(music);
-        if (bg != 'none') $('#bg').attr('src', bg);
+        if (bg != 'none') {
+            $('#bg').attr('src', '../' + bg);
+            $('#canvas').css('background-image', `url(../${bg})`);
+        }
+        // initCanvas
+        if (settings.proportion == '16:9' || settings.proportion == undefined) {
+            cvs.width = 4000;
+            cvs.height = 2250;
+            $('#canvas').css('max-width', $('body').height() / 9 * 16);
+        } else {
+            cvs.width = 3000;
+            cvs.height = 2250;
+            $('#canvas').css('max-width', $('body').height() / 4 * 3);
+        }
+        cvs = cvs.getContext('2d');
+        cvs.transform(1, 0, 0, 1, $('#canvas').attr('width') / 2, 1125);
+        cvs.save();
     }
-    
     // init UI
     $('body>*').hide();
-    $('#ui, #info, #bg').show();
-    // initCanvas
+    $('#ui, #info, #bg, #blur').show();
+    // init vars
     var cvs = document.getElementById('canvas');
     var lS = window.localStorage;
     var settings = JSON.parse(lS.settings);
-    if (settings.proportion == '16:9' || settings.proportion == undefined) {
-        cvs.width = 4000;
-        cvs.height = 2250;
-        cvs.style.maxWidth = $('body').height() / 9 * 16;
-    } else {
-        cvs.width = 3000;
-        cvs.height = 2250;
-        cvs.style.maxWidth = $('body').height() / 9 * 16;
-    }
-    cvs = cvs.getContext('2d');
-    cvs.transform(1, 0, 0, 1, $('#canvas').attr('width') / 2, 1125);
-    cvs.save();
-    // init vars
-    let time = -300, startTime = 0, lineColor = '#ffffff', runTime = setInterval(() => {time += 1}, 10),
+    let time = -300, startTime = 0, lineColor = '#ffffff',
     imgs = {
         tap: new Image(),
         drag: new Image()
@@ -39,46 +41,37 @@ function gaming() {
         lines: [],
         lineMoves: [
             {line: 0, x: 0, y: 0, deg: 0, width: 0, alpha: 1, endX: 0, endY: 0, endDeg: 0, endWidth: 10000, endAlpha: 1, startT: -200, endT: -100},
-            {line: 1, alpha: 0, endAlpha: 1, startT: 0, endT: 200},
-            {line: 1, alpha: 0, endAlpha: 0, startT: -500, endT: 0},
-            {line: 2, alpha: 0, endAlpha: 0, startT: -500, endT: 0},
-            // {line: 0, width: 10000, alpha: 1, endAlpha: 0, startT: -100, endT: 0},
-            /*{line: 1, x: 0, y: 0, deg: -90, width: 10000, endX: 200, endY: 200, endDeg: 0, endWidth: 10000, startT: 0, endT: 300},
-            {line: 2, x: 0, y: 0, deg: -45, width: 10000, endX: 200, endY: 200, endDeg: 45, endWidth: 10000, startT: 0, endT: 300},
-            {line: 0, x: 0, y: 0, deg: 0, width: 10000, alpha: 1, endX: 200, endY: 200, endDeg: 90, endWidth: 10000, startT: 0, endT: 300},
-            {line: 3, x: 0, y: 0, deg: 45, width: 10000, endX: 200, endY: 200, endDeg: 135, endWidth: 10000, startT: 0, endT: 300},
-            {line: 4, x: 0, y: 0, deg: -90, width: 10000, endX: -200, endY: -200, endDeg: 0, endWidth: 10000, startT: 0, endT: 300},
-            {line: 5, x: 0, y: 0, deg: -45, width: 10000, endX: -200, endY: -200, endDeg: 45, endWidth: 10000, startT: 0, endT: 300},
-            {line: 6, x: 0, y: 0, deg: 0, width: 10000, endX: -200, endY: -200, endDeg: 90, endWidth: 10000, startT: 0, endT: 300},
-            {line: 7, x: 0, y: 0, deg: 45, width: 10000, endX: -200, endY: -200, endDeg: 135, endWidth: 10000, startT: 0, endT: 300},
-            {line: 0, endAlpha: 0, startT: 300, endT: 400},
-            {line: 1, endAlpha: 0, startT: 300, endT: 400},
-            {line: 2, endAlpha: 0, startT: 300, endT: 400},
-            {line: 3, endAlpha: 0, startT: 300, endT: 400},
-            {line: 4, endAlpha: 0, startT: 300, endT: 400},
-            {line: 5, endAlpha: 0, startT: 300, endT: 400},
-            {line: 6, endAlpha: 0, startT: 300, endT: 400},
-            {line: 7, endAlpha: 0, startT: 300, endT: 400},*/
-            {line: 0, endY: 300, endAlpha: 1, startT: 0, endT: 100},
-            {line: 0, endDeg: 15, endY: 300, startT: 200, endT: 1000},
-            {line: 1, endDeg: -165, endY: 300, startT: 200, endT: 1000},
-            {line: 2, deg: 15, endDeg: 375, x: 220, y: 360, endX: 220, endY: 360, startT: 870, endT: 1100},
-            {line: 0, x: 0, y: 0, deg: 0, width: 10000, endX: 0, endY: 0, endDeg: 0, endWidth: 0, startT: 1200, endT: 1300}
+            {line: 1, alpha: 0, endAlpha: 0, startT: 0, endT: 1150},
+            {line: 0, endAlpha: 0, startT: 0, endT: 100},
+            {line: 2, x: 0, y: 0, alpha: 0,endX: 1300, endY: 600, deg: 0, endAlpha: 1, endDeg: 90, startT: 0, endT: 600},
+            {line: 3, x: 0, y: 0, alpha: 0,deg: 45, endX: 1300, endY: 600, endAlpha: 1, endDeg: 45 + 90, startT: 0, endT: 600},
+            {line: 4, x: 0, y: 0, alpha: 0,deg: 90, endX: 1300, endY: 600, endAlpha: 1, endDeg: 180, startT: 0, endT: 600},
+            {line: 5, x: 0, y: 0, alpha: 0,deg: 135, endX: 1300, endY: 600, endAlpha: 1, endDeg: 225, startT: 0, endT: 600},
+            {line: 2, x: 200, y: -200, endX: -1300, endY: -300, deg: 0, alpha: 1, endAlpha: 0, endDeg: 90, startT: 600, endT: 1200},
+            {line: 3, x: 200, y: -200, deg: 45, endX: -1300, endY: -300, alpha: 1, endAlpha: 0, endDeg: 45 + 90, startT: 600, endT: 1200},
+            {line: 4, x: 200, y: -200, deg: 90, endX: -1300, endY: -300, alpha: 1, endAlpha: 0, endDeg: 180, startT: 600, endT: 1200},
+            {line: 5, x: 200, y: -200, deg: 135, endX: -1300, endY: -300, alpha: 1, endAlpha: 0, endDeg: 225, startT: 600, endT: 1200},
+            {line: 1, alpha: 0, endAlpha: 1, startT: 1100, endT: 1300},
+            {line: 1, x: 0, y: 0, deg: 0, alpha: 1, endX: -1800, endY: 0, endDeg: 90, startT: 1150, endT: 1400},
+            {line: 0, endY: 600, startT: 1150, endT: 1300},
+
+            {line: 0, x: 0, y: 0, deg: 0, width: 10000, alpha: 1, endX: 0, endY: 0, endDeg: 0, endWidth: 0, endAlpha: 1, startT: 1900, endT: 2000},
         ],
         notes: [],
         noteMoves: [
-            {line: 0, note: 0, x: 0, y: 1000, endX: 0, endY: 'down', startT: 200, endT: 400},
-            {line: 1, note: 1, x: 250, y: 1000, endX: 50, endY: 'down', startT: 250, endT: 450},
-            {line: 0, note: 3, x: 600, y: 1000, endX: 200, endY: 500, startT: 450, endT: 600},
-            {line: 1, note: 2, x: 400, y: 1000, endX: 400, endY: 'down', startT: 600, endT: 800},
-            {line: 0, note: 3, x: 200, y: 500, endX: 200, endY: 'down', startT: 800, endT: 900},
-            {line: 0, note: 4, x: 900, y: 2400, endX: 200, endY: 'down', type: 'drag', startT: 800, endT: 900}
+            {line: 0, note: 0, x: 0, y: 1800, endX: 0, endY: 'down', startT: 1200, endT: 1350},
+            {line: 0, note: 1, x: -500, y: 1800, endX: 500, endY: 'down', startT: 1300, endT: 1450},
+            {line: 0, note: 2, x: -500, y: 1800, endX: 500, endY: 'down', type: 'drag', startT: 1700, endT: 1800},
+            {line: 0, note: 3, x: -500, y: 1800, endX: 500, endY: 'down', type: 'drag', startT: 1650, endT: 1750},
+            {line: 0, note: 4, x: -500, y: 1800, endX: 500, endY: 'down', type: 'drag', startT: 1600, endT: 1700},
+            {line: 1, note: 5, x: 0, y: 1800, endX: 0, endY: 'down', startT: 1200, endT: 1400},
+            {line: 1, note: 6, x: 0, y: 1800, endX: 0, endY: 'down', startT: 1300, endT: 1450},
+            {line: 1, note: 7, x: 0, y: 1800, endX: 0, endY: 'down', startT: 1600, endT: 1700},
         ],
     }
-    imgs.tap.src = '../asset/img/ui/tap.svg';
-    imgs.drag.src = '../asset/img/ui/drag.svg';
-    console.log(imgs.tap);
-    loadGame({music: '../asset/audio/music/test.wav'});
+    loadGame({music: '../asset/audio/music/test.wav', bg: 'asset/img/bg/img0.webp'});
+    imgs.tap.src = '../asset/img/ui/Tap2.png';
+    imgs.drag.src = '../asset/img/ui/drag.png';
     audio.oncanplaythrough = () => {
         console.log('loaded');
         function distance(x, y, endX, endY){
@@ -87,8 +80,11 @@ function gaming() {
         function addLine(context, x, y, deg, width, alpha, notes) {
             spectral.lines.push(new line(context, x, y, deg, width, alpha));
         }
-        function removeLine(arrayNum) {
-            spectral.lines.remove(arrayNum);
+        function removeLine(from, to) {
+            spectral.lines.remove(from, to);
+        }
+        function removeNote(from, to) {
+            spectral.notes.remove(from, to);
         }
         function addNote(line, context, x, y, type, trueNote) {
             spectral.notes.push(new note(line, context, x, y, type, trueNote));
@@ -165,11 +161,11 @@ function gaming() {
                     this.context.setTransform(1, 0, 0, 1, $('#canvas').attr('width') / 2 + spectral.lines[this.line].x, 1125 + spectral.lines[this.line].y);
                     this.context.rotate(spectral.lines[this.line].deg * Math.PI / 180);
                     this.context.globalAlpha = 1;
-                    this.context.drawImage(noteImg, this.x - 150, 0 - (this.y - 15), 300, 30);
+                    this.context.drawImage(noteImg, this.x - 150, 0 - (this.y + 12.5), 300, 25);
                     this.context.restore();
                 }
             }
-            move({line, x = this.x, y = this.y, endX = 0, endY = 0, startT, endT} = {}) {
+            move({line, x = this.x, y = this.y, endX = 0, endY = 0, startT, endT, type = 'tap'} = {}) {
                 // console.log(x, y, deg, width, endX, endY, endDeg, endWidth, startT, endT);
                 let deleteNote = false;
                 if (endY == 'down') {
@@ -179,11 +175,19 @@ function gaming() {
                 if (line !== undefined) {
                     this.line = line;
                 }
+                switch(type) {
+                    case 'tap':
+                        this.type = 'tap';
+                        break;
+                    case 'drag':
+                        this.type = 'drag';
+                        break;
+                }
                 if (time >= startT && time <= endT) {
                     let vx = (endX - x) / (endT - startT);
                     let vy = (endY - y) / (endT - startT);
                     this.x = x + (time - startT) * vx;
-                    this.y = y - 2 + (time - startT) * vy;
+                    this.y = y + (time - startT) * vy;
                 }
                 if (time > endT) {
                     this.x = endX;
@@ -198,18 +202,16 @@ function gaming() {
         }
         class gameBoard {
             constructor() {
-                this.startTime;
                 this.init();
                 this.process();
             }
             init() {
                 addLine(cvs, 3000, 3000, 0, 1);
-                addNote(0, cvs, 0, 1000, 'tap', true);
+                addNote(0, cvs, 0, 5000, 'tap', true);
             }
-            removeAllLine() {
-                for (let i = 1; i < spectral.lines.length; i++) {
-                    removeLine(i);
-                }
+            removeAll() {
+                removeLine(1, spectral.lines.length);
+                removeNote(0, spectral.notes.length);
             }
             // 动画循环（帧率）
             process(now){
@@ -219,6 +221,9 @@ function gaming() {
                 }
                 let seconds = (now - startTime) / 1000;
                 startTime = now;
+                if (seconds) {
+                    time += seconds * 100;
+                }
                 cvs.clearRect(-10000, -10000, 20000, 20000);for (let i = 0; i < spectral.lineMoves.length; i++) {
                     while (!spectral.lines[spectral.lineMoves[i].line]) {
                         addLine();
@@ -231,30 +236,33 @@ function gaming() {
                     while (!spectral.notes[spectral.noteMoves[i].note]) {
                         addNote(spectral.noteMoves[i].line, undefined, undefined, undefined, spectral.noteMoves[i].type);
                     }
-                    if (spectral.notes[spectral.noteMoves[i].note].move({line: spectral.noteMoves[i].line, x: spectral.noteMoves[i].x, y: spectral.noteMoves[i].y, endX: spectral.noteMoves[i].endX, endY: spectral.noteMoves[i].endY, startT: spectral.noteMoves[i].startT, endT: spectral.noteMoves[i].endT})) {
+                    if (spectral.notes[spectral.noteMoves[i].note].move({line: spectral.noteMoves[i].line, x: spectral.noteMoves[i].x, y: spectral.noteMoves[i].y, endX: spectral.noteMoves[i].endX, endY: spectral.noteMoves[i].endY, startT: spectral.noteMoves[i].startT, endT: spectral.noteMoves[i].endT, type: spectral.noteMoves[i].type})) {
                         spectral.noteMoves.remove(i);
                     }
                 }
-                for(var i = 0; i < spectral.lines.length; i++) {
-                    spectral.lines[i].draw(cvs);
+                if (time >= 0) {
+                    for(var i = 0; i < spectral.lines.length; i++) {
+                        spectral.lines[i].draw(cvs);
+                    }
+                    for(var i = 0; i < spectral.notes.length; i++) {
+                        spectral.notes[i].draw(cvs);
+                    }
+                } else {
+                    spectral.lines[0].draw(cvs);
                 }
-                for(var i = 0; i < spectral.notes.length; i++) {
-                    spectral.notes[i].draw(cvs);
-                }
-                
+                console.log(spectral.notes);
                 // console.log('canvas refresh', now, startTime, seconds);
                 // 动画循环（帧率）
                 var gameLoop = window.requestAnimationFrame(this.process.bind(this));
-                if (time >= 1400) {
+                if (time >= 2100) {
                     window.cancelAnimationFrame(gameLoop)
                 }
-                if (time >= 1200) {
-                    this.removeAllLine();
+                if (time >= 1900) {
+                    this.removeAll();
                 }
             }
         }
-
-        var gameMain = new gameBoard();
+        new gameBoard();
     };
 }
 $(() => {
